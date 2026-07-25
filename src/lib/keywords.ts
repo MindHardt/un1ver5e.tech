af101485c819149aff9b189e6632b60275957663
MindHardt <igorbabin2002@gmail.com>
2026-07-25 12:21:07 +0600

    Added keywords deduplication

--- src/lib/keywords.ts
+++ src/lib/keywords.ts
@@ -1,7 +1,8 @@
 import extraKeywords from "@/content/keywords.json";
 import { getCollection } from "astro:content";
+import { distinctBy } from "./collections";
 
 export async function getKeywords() : Promise<readonly string[]> {
     const techNames = new Set((await getCollection('techs')).flatMap(x => [x.data.name, x.id, ...x.data.aliases]));
-    return [...extraKeywords, ...techNames].sort();
+    return [...extraKeywords, ...distinctBy(techNames, x => x.toUpperCase())].sort();
 }
